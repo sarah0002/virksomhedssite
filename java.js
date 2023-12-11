f8d85e3555d26034a6c188a92f5cdec41b9c9771
Sophie <saclassichouse@gmail.com>
2023-12-11 10:39:34 +0100

    burgermenu

--- java.js
+++ java.js
@@ -1,60 +1,62 @@
 /** @format */
 
 let loadMoreBtn = document.querySelector("#load-more");
-let currentItem = 4;
+if (loadMoreBtn !== null) {
+  let currentItem = 4;
 
-loadMoreBtn.onclick = () => {
-  console.log("Button clicked"); // Tjek om denne besked vises i konsollen
+  loadMoreBtn.onclick = () => {
+    console.log("Button clicked"); // Tjek om denne besked vises i konsollen
 
-  let boxes = [
-    ...document.querySelectorAll(".container_team .box-container .box"),
-  ];
+    let boxes = [
+      ...document.querySelectorAll(".container_team .box-container .box"),
+    ];
 
-  for (let i = currentItem; i < currentItem + 4 && i < boxes.length; i++) {
-    boxes[i].style.display = "inline-block";
-  }
+    for (let i = currentItem; i < currentItem + 4 && i < boxes.length; i++) {
+      boxes[i].style.display = "inline-block";
+    }
 
-  currentItem += 4;
+    currentItem += 4;
 
-  if (currentItem >= boxes.length) {
-    loadMoreBtn.style.display = "none";
-  }
-};
+    if (currentItem >= boxes.length) {
+      loadMoreBtn.style.display = "none";
+    }
+  };
+}
 
 // Søgeflet \\
 
-document.addEventListener("DOMContentLoaded", function () {
-  var searchContainer = document.getElementById("searchContainer");
-  var searchIcon = document.getElementById("sog_img");
+// document.addEventListener("DOMContentLoaded", function () {
+//   var searchContainer = document.getElementById("searchContainer");
+//   var searchIcon = document.getElementById("sog_img");
 
-  document.addEventListener("click", function (event) {
-    if (
-      searchContainer.classList.contains("active") &&
-      event.target !== searchIcon &&
-      !searchContainer.contains(event.target) &&
-      event.target !== document.getElementById("searchInput")
-    ) {
-      toggleSearch();
-    }
-  });
+//   document.addEventListener("click", function (event) {
+//     if (
+//       searchContainer.classList.contains("active") &&
+//       event.target !== searchIcon &&
+//       !searchContainer.contains(event.target) &&
+//       event.target !== document.getElementById("searchInput")
+//     ) {
+//       toggleSearch();
+//     }
+//   });
 
-  searchIcon.addEventListener("click", function (event) {
-    event.stopPropagation();
-    toggleSearch();
-  });
+//   searchIcon.addEventListener("click", function (event) {
+//     event.stopPropagation();
+//     toggleSearch();
+//   });
 
-  // Tilføj event listener til søgeknappen
-  document
-    .getElementById("searchButton")
-    .addEventListener("click", function (event) {
-      event.preventDefault(); // Forhindrer standardformularindsendelse
-      search();
-    });
-});
+// Tilføj event listener til søgeknappen
+// document
+//   .getElementById("sog_img")
+//   .addEventListener("click", function (event) {
+//     event.preventDefault(); // Forhindrer standardformularindsendelse
+//     search();
+//   });
+// });
 
 function toggleSearch() {
-  var searchContainer = document.getElementById("searchContainer");
-  searchContainer.classList.toggle("active");
+  // var searchContainer = document.getElementById("searchContainer");
+  // searchContainer.classList.toggle("active");
 }
 
 function search() {
